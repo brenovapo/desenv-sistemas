a5bb40a9dddef5347caf324a0e75b9e1bb14211a
unknown <brenoronaldomatos2014@gmail.com>
2025-06-23 09:00:11 -0300

    trabalhando2

--- backend/logicaProgramacao/01_variaveis.py
+++ backend/logicaProgramacao/01_variaveis.py
@@ -8,25 +8,25 @@ print("Idade", idade)
 print("Altura", altura)
 print("Tem carteira?", tem_carteira)
 
-animal = "Leão"
+animal = "Leão"                          #string
 trabalho = "bruto"
 
 print("animal", animal)
 print("trabalho", trabalho)
 
-numero = 7
+numero = 7                               #boat
 ano = 2025
 
 print("numero", numero)
 print("ano", ano)
 
-peso = 60.5
+peso = 60.5                             #float    
 temperatura = 29.1
 
 print("peso", peso)
 print("temperatura", temperatura)
 
-tem_fome = False
+tem_fome = False                            #boleano
 vitor_burro = True
 
 print("Tem fome?", tem_fome)
