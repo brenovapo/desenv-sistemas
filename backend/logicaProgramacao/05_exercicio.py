1ed6b75cb21899e0d156e1c148c155f0d79b6b44
breno <brenoronaldomatos2014@gmail.com>
2025-07-28 08:50:28 -0300

    trabalho6

---
+++ backend/logicaProgramacao/05_exercicio.py
@@ -0,0 +1,16 @@
+
+
+soma = 0
+#for
+for i in range(21):
+    soma += i
+print("soma dos valores:", soma)
+
+
+#pedir ao usuario  valores e soma-los enquanto o usuario nao digitar zero
+
+
+
+    
+
+
