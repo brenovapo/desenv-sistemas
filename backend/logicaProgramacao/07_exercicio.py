d2fb5fbac5aba118c37fbcca57af5010b8110d42
breno <brenoronaldomatos2014@gmail.com>
2025-07-29 09:32:49 -0300

    msg

---
+++ backend/logicaProgramacao/07_exercicio.py
@@ -0,0 +1,22 @@
+senhacorreta = input("configure a senha")
+senha = input("digite a senha")
+
+
+while senha != senhacorreta:
+    print("senha incorreta") 
+    senha = input("digite a senha novamente")
+print("senha correta")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ backend/logicaProgramacao/08_exercicio.py
@@ -0,0 +1,15 @@
+breno = input("digite seu nome ")
+nome_errado = input("configure o nome")
+senha = input("digite sua senha ")
+senha_errado = input("configure seu salario")
+float (input("Digite seu salário "))
+
+
+while senha != senha_errado:
+    print("senha incorreta") 
+    senha = input("digite a senha novamente")
+print("bem vindo")
+
+
+ #pedir nome e senha ao usuario,  mostrar "Bem vindo" quando acertar a senha, e o nome.
+ #-Após, pedir o salario do usuario  -Mostrar salario anual  -se o salario anual for maior que 100 mil mostrar mensagem "Rico"
