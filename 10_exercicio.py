cdf49bf3bc9904669891ae36ddbc2c2d363caa70
breno <brenoronaldomatos2014@gmail.com>
2025-08-19 09:37:50 -0300

    msg

---
+++ 10_exercicio.py
@@ -0,0 +1,4 @@
+alunos = ["breno", "rockbala", "carlos", "daniela", "eduardo", "fernanda", "gustavo", "helena", "igor", "juliana"]
+
+for aluno in alunos:
+    print("Olá " + aluno)
---
+++ backend/logicaProgramacao/09_exercicios.py
@@ -0,0 +1,17 @@
+#append pro final
+#susert 1,1700,00
+
+
+tarefas = [
+    "lavar a louça"
+    "estudar inglês"
+    "ir ao shopping"
+    "sair com os amigos"
+    "jogar bola"
+
+]
+
+tarefas.insert(2, "ir viajar")
+
+tarefas.remove("lavar a louça")
+
