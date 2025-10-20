01e97e1641ca71572580d84c6d5178e5717fa782
breno <brenoronaldomatos2014@gmail.com>
2025-10-20 08:47:59 -0300

    msg

---
+++ gerenciador_tarefas/config/tarefas/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path 
+from .imporviews
+urlpatterns = [
+
+    path('',views.listar_tarefas, name = 'listar_tarefas')
+]
--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Tarefa
+from django.http import HttpResponse
 
-# Create your views here.
+def listar_tarefas(request):
+    tarefas_salvas = Tarefas.objects.all()
+    contexto = [
+                    "minhas_tarefas": tarefas_salvas
+    ]
+    return render (request, 'tarefas/lista.html', contexto)
+
+    
